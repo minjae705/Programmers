f58f75db52eb40237cba8dda94f3036100fdc3af
민재고 <59470650+minjae705@users.noreply.github.com>
2021-06-22 22:57:54 +0900

    Update K번째수.cpp

--- Level1/K번째수.cpp
+++ Level1/K번째수.cpp
@@ -17,5 +17,12 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
         sort(tmp.begin(),tmp.end());
         answer.push_back(tmp[k]);
     }
+    
+//         for(int i = 0; i < commands.size(); i++) {
+//         tmp = array;
+//         sort(tmp.begin() + commands[i][0] - 1, tmp.begin() + commands[i][1]);
+//         answer.push_back(tmp[commands[i][0] + commands[i][2]-2]);
+//     } 
+    
     return answer;
 }
