549856c7d05db81e331ad847869a7495db5f9c52
민재고 <59470650+minjae705@users.noreply.github.com>
2021-06-23 16:10:35 +0900

    Create 하샤드수.cpp

---
+++ Level1/하샤드수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+bool solution(int x) {
+    string s = to_string(x);
+    int sum = 0;
+    for(int i = 0; i < s.length(); i++)
+        sum += s[i]-'0';
+    if(x % sum == 0)
+        return true;
+    else
+        return false;
+  
+// return (x % sum == 0);
+}
