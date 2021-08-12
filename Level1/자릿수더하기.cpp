ead92e78132ff80ee6d875bd5b3f307562415bed
민재고 <59470650+minjae705@users.noreply.github.com>
2021-08-12 15:45:03 +0900

    Create 자릿수더하기.cpp

---
+++ Level1/자릿수더하기.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    string str = to_string(n);
+    for(int i=0; i<str.size(); i++)
+        answer += str[i] - '0';
+    return answer;
+}
