f0654d2df9919693fb5664a22f81d5fac31610fb
민재고 <59470650+minjae705@users.noreply.github.com>
2021-09-03 16:17:27 +0900

    Update 메뉴 리뉴얼.py

--- 2021 KAKAO BLIND RECRUITMENT/메뉴 리뉴얼.py
+++ 2021 KAKAO BLIND RECRUITMENT/메뉴 리뉴얼.py
@@ -1,23 +1,18 @@
-from itertools import combinations 
+from itertools import combinations
+from collections import Counter
+
 def solution(orders, course):
     answer = []
-    single = []
-    for i in range(65,91):
-        single.append(chr(i))
     for size in course:
-        combi = []
-        for com in combinations(single, size):
-            combi.append(''.join(com))
-        cnt = [0 for i in range(len(combi))]
-        for j in range(len(combi)):
-            for order in orders:
-                tmpord = set(order)
-                tmpcom = set(combi[j])
-                if tmpord & tmpcom == tmpcom:
-                    cnt[j] += 1
-        tmp = max(cnt)
-        for c in range(len(cnt)):
-            if cnt[c] == tmp and tmp >= 2:
-                answer.append(combi[c])
-    answer = sorted(answer)                
+        combilist = []
+        for order in orders:
+            combilist += list(map(''.join, combinations(sorted(order), size)))
+        if len(combilist) == 0:
+            continue
+        cntcombilist = Counter(combilist).most_common()
+        cnt = cntcombilist[0][1]
+        for c in cntcombilist:
+             if c[1] == cnt and c[1] >= 2:
+                answer.append(c[0])   
+    answer = sorted(answer)
     return answer
